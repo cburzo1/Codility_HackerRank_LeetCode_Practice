7a0ea9f46ac6dc41f03545cf53e087b05e9b3581
cburzo1 <gm.c.burzo@gmail.com>
2025-06-23 16:22:46 -0400

    Working version of problem

--- Set_1_Realistic_HackerRank-Style _Implementation-Heavy/format_licence_key/main.py
+++ Set_1_Realistic_HackerRank-Style _Implementation-Heavy/format_licence_key/main.py
@@ -1,6 +1,29 @@
 import re
 
-def format_licence_key(S, K):
+def format_license_key(S, K):
+    s = re.sub(r'[^a-zA-Z0-9]', '', S).upper()
+
+    arr = []
+    count = 0
+    temp = ""
+
+    # Go backwards through the string
+    for i in range(len(s) - 1, -1, -1):
+        temp = s[i] + temp
+        count = count + 1
+
+        if count == K:
+            arr.insert(0, temp)
+            temp = ""
+            count = 0
+    
+    # Add remaining chars if any
+    if temp != "":
+        arr.insert(0, temp)
+
+    return "-".join(arr)
+
+'''def format_licence_key(S, K):
     f_str = ""
 
     s = re.sub(r'[^a-zA-Z0-9]', '', S).upper()
@@ -29,7 +52,7 @@ def format_licence_key(S, K):
 
         print(arr, ns)
 
-    return f_str
+    return f_str'''
 
 format_licence_key("2-4A0r7-4k", 4)
 format_licence_key("2-4A0r7-4k", 3)
