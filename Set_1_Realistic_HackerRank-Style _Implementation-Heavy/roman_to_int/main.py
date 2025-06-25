0b6205c4445d4433ef4a99df348d0d47789c0111
cburzo1 <gm.c.burzo@gmail.com>
2025-06-25 16:00:17 -0400

    tried roamn_to_int

--- Set_1_Realistic_HackerRank-Style _Implementation-Heavy/roman_to_int/main.py
+++ Set_1_Realistic_HackerRank-Style _Implementation-Heavy/roman_to_int/main.py
@@ -1,6 +1,25 @@
 def roman_to_int(str):
-    to_int = 0
+    ro_in = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D": 500,
+        "M":1000
+    }
 
-    
+    s = list(str)
+    total = 0
 
-    return to_int
+    arr = []
+
+    for i in range(0, len(s)):
+        if s[i] in ro_in:
+            total = total + ro_in.get(s[i])
+        else:
+            return "not valid roman"
+
+    return total
+
+print(roman_to_int("III"))
