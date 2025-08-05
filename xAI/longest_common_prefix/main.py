ccb9bd7fad4457bd80ceab8cca782137eed4bfad
cburzo1 <gm.c.burzo@gmail.com>
2025-08-05 15:31:12 -0400

    conitnue lcp

--- xAI/longest_common_prefix/main.py
+++ xAI/longest_common_prefix/main.py
@@ -1,17 +1,7 @@
 def longest_common_prefix(l_s):
-
     aux = []
 
-    for i in range(0, len(l_s)):
-        if len(l_s[i]) > len(aux):
-            aux = l_s[i]
-
-    print(aux)
-
-    for i in range(0, len(l_s)):
-        s_l = list(l_s[i])
-        for j in range(0, len(s_l)):
-            if s_l[j] not in au
+    
 
 print(longest_common_prefix(["flower","flow","flight"]))  # → "fl"
 #print(longest_common_prefix(["dog","racecar","car"])  )   # → ""
