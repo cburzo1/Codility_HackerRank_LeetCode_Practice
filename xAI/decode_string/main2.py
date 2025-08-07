63d355b511213e2a3cd6fefa4b902ed19b840060
cburzo1 <gm.c.burzo@gmail.com>
2025-08-07 07:31:37 -0400

    added second decode

--- xAI/decode_string/main2.py
+++ xAI/decode_string/main2.py
@@ -1,9 +1,25 @@
 def decode_string(s):
-    print(s)
+    s_l = list(s)
+    stk = []
+
+    for i in range(0, len(s_l)):
+        if s_l[i] == "]":
+            t_s = ""
+            s_e = stk.pop()
+            while s_e != "[":
+                t_s += s_e
+                s_e = stk.pop()
+
+            print(t_s[::-1])
+
+            print(stk)
+
+        else:
+            stk.append(s_l[i])
 
 print(decode_string("3[a]2[bc]")) # "aaabcbc"
-print(decode_string("3[a2[c]]")) # "accaccacc"
-print(decode_string("2[abc]3[cd]ef")) # "abcabccdcdcdef"
-print(decode_string("10[a]")) # "aaaaaaaaaa"
-print(decode_string("")) # ""
-print(decode_string("2[]")) # ""
+#print(decode_string("3[a2[c]]")) # "accaccacc"
+#print(decode_string("2[abc]3[cd]ef")) # "abcabccdcdcdef"
+#print(decode_string("10[a]")) # "aaaaaaaaaa"
+#print(decode_string("")) # ""
+#print(decode_string("2[]")) # ""
