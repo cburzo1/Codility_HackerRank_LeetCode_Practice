1bdeff5fd1bfa8a11a81a279011d89027f86d176
cburzo1 <gm.c.burzo@gmail.com>
2025-08-14 07:32:06 -0400

    merging intervals continued

--- xAI/merging_intervals/main.py
+++ xAI/merging_intervals/main.py
@@ -1,14 +1,28 @@
 def merging_intervals(mx):
     aux = []
 
-    for i in range(1, len(mx)):
+    '''for i in range(1, len(mx)):
         if mx[i - 1][1] >= mx[i][0]:
             aux.append([mx[i - 1][0], mx[i][1]])
         else:
-            aux.append(mx[i])
+            aux.append(mx[i])'''
+
+    #sort by y
+    for i in range(len(mx), -1, -1):
+        for j in range(1, i):
+            if mx[j - 1][1] > mx[j][1]:
+                temp = mx[j]
+                mx[j] = mx[j - 1]
+                mx[j - 1] = temp
+
+    print(mx)
+
+    # start at mx[0] and only check the next pair, if the next pair doesnt pass,
+    # merge stay on this index though and check the next one, only moving on if it passes
 
     return aux
 
-print(merging_intervals([[1,3],[2,6],[8,10],[15,18]]))
-print(merging_intervals([[1,4],[4,5]]))
-print(merging_intervals([[1,3],[2,5],[4,8]]))
+print(merging_intervals([[2,6],[1,3],[9,18],[5,10]])) # always check next before advancing
+# [1,6] [5, 10] [9, 18] -> [1,10], [9,18] -> [1, 18]
+#print(merging_intervals([[1,4],[4,5]]))
+#print(merging_intervals([[1,3],[4,8],[2,5]]))
