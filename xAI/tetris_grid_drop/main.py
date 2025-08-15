5334dcf4c5cfa8a6b37bcc2e392e320264fe9f3c
cburzo1 <gm.c.burzo@gmail.com>
2025-08-15 13:56:07 -0400

    tetric grid drop start over

--- xAI/tetris_grid_drop/main.py
+++ xAI/tetris_grid_drop/main.py
@@ -1,22 +1,7 @@
 def tetris_grid_drop(mx_grid, mx_piece):
     idx = 0
 
-    print(mx_grid, mx_piece)
-
-    piece_width = 0
-    count = 0
-
-    for x in range(0, len(mx_piece)):
-        for y in range(0, len(mx_piece)):
-            if mx_piece[x][y] == 1:
-                count += 1
-
-        if count > piece_width:
-            piece_width = count
-            count = 0
-
-    print(piece_width)
-
+    
 
     return idx
 
@@ -31,3 +16,54 @@ print(tetris_grid_drop([
     [1, 1, 1],
     [0, 0, 0]
 ]))
+
+'''print(tetris_grid_drop( [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [1, 1, 1, 0]
+],
+
+[
+    [1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]
+]))
+
+print(tetris_grid_drop([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+,
+[
+    [1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]
+]))
+
+print(tetris_grid_drop([
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0]
+]
+
+,[
+    [1, 1, 1],
+    [1, 0, 1],
+    [0, 0, 0]
+]))
+
+print(tetris_grid_drop([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [1, 1, 1, 0],
+    [1, 1, 1, 0]
+]
+,
+[
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+]))'''
